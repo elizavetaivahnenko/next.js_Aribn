cd685bafe07612c69f5ed25ffaa4b286e6b40556
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2023-05-28 19:14:06 +0200

    registration with github, waiting on changes, errors with aatar and db-account

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -55,7 +55,7 @@ const LoginModal = () => {
 		<div className="flex flex-col gap-4 mt-3">
 			<hr />
 			<Button outline label="Continue with Google" icon={FcGoogle} onClick={() => { }} />
-			<Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => { }} />
+			<Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn('github')} />
 			<div className="text-neutral-500 text-center mt-4 font-light">
 				<div className="flex justify-center flex-row items-center gap-2">
 					<div>
