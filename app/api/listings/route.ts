acbfe25a98046b5175607bea5fb73861411747b6
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2023-06-02 18:42:25 +0200

    fetching listings with server component attention with database in updateAt

---
+++ app/api/listings/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(
+  request: Request, 
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { 
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+   } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
+    }
+  });
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      lacatiomValues: location.value,
+      price: parseInt(price, 10),
+		userId: currentUser.id,
+		updatedAt: 'problem with updatedAt it has not bee null, pay attention! bilding database',
+    }
+  });
+
+  return NextResponse.json(listing);
+}
